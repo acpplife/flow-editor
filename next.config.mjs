86ea9a3fb1cf0399eadef9982dfd161180dc841d
wangzhichang <18329110825@163.com>
2024-08-18 15:35:15 +0800

    commit test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // 其他页面路径
+    }
+  },
+  basePath: '/flow-editor',
+  assetPrefix: '/flow-editor/',
 };
 
 export default nextConfig;
