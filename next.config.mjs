20c95d50ecff707852b403f04abd6fab7da5b733
wangzhichang <18329110825@163.com>
2024-08-18 16:14:20 +0800

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      // 其他页面路径
-    }
-  },
   basePath: '/flow-editor',
   assetPrefix: '/flow-editor/',
 };
